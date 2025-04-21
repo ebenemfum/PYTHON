2d11da8a6d6c0133c416db42e2828b9f25d2b4e7
Ebenezer Amoah <ebenezer.amoah@arkesel.com>
2025-04-21 14:51:41 +0100

    DAY 1 COMPLETE 🍆🤤

---
+++ FIRST DAY/task.py
@@ -0,0 +1,31 @@
+# """ Printing Practice
+# Write a program that uses print statements to print the following recipe into the Output console. 
+# The text to print is already there, you just need to make it into code. 
+# Your code should print all five lines exactly the same as the example output below. 
+# Make sure that you don't change any of these existing text as everything, punctuation and casing all need to match!
+# 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+
+
+# NOTES: 
+# "(\n) - New line character. 
+# "(+) - Concatentation.
+# "input() - Accept user input"
+# "len()" - Length
+
+# """
+
+# """ print("1. Mix 500g of Flour, 10g Yeast \n and 300ml Water in a bowl.")
+# print("2. Knead the dough for 10 minutes.")
+# print("3. Add 3g of Salt.")
+# print("4. Leave to rise for 2 hours.")
+# print("5. Bake at" + " 200 degrees C for 30 minutes.")
+#  """
+ 
+# print("Hello " + input("What is your name ")+"!")
+name = input("What is your name? ")
+length=len(name)
+print("Name:"+name+"\n Length:"+str(length))
